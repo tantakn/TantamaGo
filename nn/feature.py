24b9b0bb9b0c08ac950e5f4ade454b98bec7434e
tantakn <gjmdjptgjmdjpt@gmail.com>
2024-10-07 19:00:09 +0900

    Refactor generate_input_planes_with_semeai function to include semeai data in neural network input

--- nn/feature.py
+++ nn/feature.py
@@ -59,6 +59,62 @@ def generate_input_planes(board: GoBoard, color: Stone, sym: int=0) -> np.ndarra
 
     return input_data
 
+def generate_input_planes_with_semeai(board: GoBoard, color: Stone, sym: int=0) -> np.ndarray:
+    """ニューラルネットワークの入力データ（説明変数）を生成する。
+
+    Args:
+        board (GoBoard): 碁盤の情報。
+        color (Stone): 手番の色。
+        sym (int, optional): 対称形の指定. Defaults to 0.
+
+    Returns:
+        numpy.ndarray: ニューラルネットワークの入力データ。
+    """
+
+    # 下の方に例がある。
+
+    board_data = board.get_board_data(sym)
+    """空点は0, 黒石は1, 白石は2の盤面の2次元リスト。"""
+    board_size = board.get_board_size()
+    # 手番が白の時は石の色を反転する.
+    if color is Stone.WHITE:
+        board_data = [datum if datum == 0 else (3 - datum) for datum in board_data]
+
+    # 碁盤の各交点の状態
+    #     空点 : 1枚目の入力面
+    #     自分の石 : 2枚目の入力面
+    #     相手の石 : 3枚目の入力面
+    board_plane = np.identity(3)[board_data].transpose()
+
+    # 直前の着手を取得
+    _, previous_move, _ = board.record.get(board.moves - 1)
+
+    # 直前の着手の座標
+    #     着手 : 4枚目の入力面
+    #     パス : 5枚目の入力面
+    if board.moves > 1 and previous_move == PASS:
+        history_plane = np.zeros(shape=(1, board_size ** 2))
+        pass_plane = np.ones(shape=(1, board_size ** 2))
+    else:
+        previous_move_data = [1 if previous_move == board.get_symmetrical_coordinate(pos, sym) else 0 for pos in board.onboard_pos]
+        history_plane = np.array(previous_move_data).reshape(1, board_size**2)
+        pass_plane = np.zeros(shape=(1, board_size ** 2))
+
+    # 手番の色 (6番目の入力面)
+    # 黒番は1、白番は-1。現局面に打つ手番の色を示す。
+    color_plane = np.ones(shape=(1, board_size**2))
+    if color == Stone.WHITE:
+        color_plane = color_plane * -1
+
+    semeai_data = board.get_liberty_data(sym)
+    semeai_plane = [semeai_data[x:x+9] for x in range(0, board_size**2, board_size)]
+    semeai_plane = np.array(semeai_plane).reshape(1, board_size**2)
+
+    input_data = np.concatenate([board_plane, history_plane, pass_plane, color_plane, semeai_plane]) \
+        .reshape(7, board_size, board_size).astype(np.float32) # pylint: disable=E1121
+
+    return input_data
+
 
 def generate_target_data(board:GoBoard, target_pos: int, sym: int=0) -> np.ndarray:
     """教師あり学習で使用するターゲットデータ（目的変数）を生成する。
