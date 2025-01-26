dfa7e36635eae5c61379f800c8c7061f51db6b4c
tantakn <gjmdjptgjmdjpt@gmail.com>
2025-01-26 11:33:32 +0900

    fix: コマンドライン引数を更新し、時間管理の制限を調整

--- mcts/time_manager.py
+++ mcts/time_manager.py
@@ -76,7 +76,8 @@ class TimeManager:
         if self.mode == TimeControl.TIME_CONTROL:
             remaining_time = self.remaining_time[0] \
                 if color is Stone.BLACK else self.remaining_time[1]
-            self.time_limit = remaining_time / 10.0
+            self.time_limit = remaining_time / 80.0
+            # self.time_limit = remaining_time / 60.0
             threshold = int(self.search_speed * self.time_limit)
             return threshold if threshold > 0 else 1
         return int(self.constant_visits)
