7b7bc708940b23b744505d94ba1596c68c7eea80
tantakn <gjmdjptgjmdjpt@gmail.com>
2025-01-28 00:10:15 +0900

    aaa

--- mcts/time_manager.py
+++ mcts/time_manager.py
@@ -77,7 +77,7 @@ class TimeManager:
             remaining_time = self.remaining_time[0] \
                 if color is Stone.BLACK else self.remaining_time[1]
             self.time_limit = remaining_time / 80.0
-            # self.time_limit = remaining_time / 60.0
+            # self.time_limit = remaining_time / 10.0 # 9路用の設定
             threshold = int(self.search_speed * self.time_limit)
             return threshold if threshold > 0 else 1
         return int(self.constant_visits)
--- zzcpp.cpp
+++ zzcpp.cpp
@@ -1,10 +1,40 @@
-#include <iomanip>
-#include <iostream>
-#include <cmath>
+#include "./myMacro.hpp"
 
-int main()
-{
-    double num = 3.1415926535;
-    int decimal = static_cast<int>((num - std::floor(num)) * 1000 + 0.5);
-    std::cout << decimal << std::endl;
+// int main()
+// {
+//     vector<string> v;
+//     string s;
+//     string ss = "asdf zxcv";
+//     // stringstream ss{ss}; 
+//     while (getline(ss, s, ' ')) {
+//         v.push_back(s);
+//     }
+
+//     print(v);
+// }
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+int main(void){
+    vector<string> v;                  // 分割した文字列を格納するvector
+    string str, s;                      
+
+    // str = "A B C D E"     ;             // 分割対象の文字列str
+
+    getline(cin, str);
+
+    stringstream ss{str};              // 入出力可能なsstreamに変換
+
+    print(str);
+
+    while ( getline(ss, s, ' ') ){     // スペース（' '）で区切って，格納
+        v.push_back(s);
+    }
+    
+    for (const string& s : v){         // vの中身を出力
+        cout << s << endl;
+    }
 }
